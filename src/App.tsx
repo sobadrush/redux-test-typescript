11b3b4c57851903c443713ea7542ce106c1929c8
sobadrush <sobadrush@icloud.com>
2020-11-16 22:21:21 +0800

    參考資料完成

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IMyProps {
+    empAge?: number;
 }
 
-export default App;
+interface IMyState {
+    empName?: string;
+}
+
+export default class App extends React.Component<IMyProps, IMyState>{
+
+    constructor(props: IMyProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+              </a>
+                </header>
+            </div>
+        );
+    }
+}
